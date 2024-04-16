88bf32d8f23d8ba171313212c08f231133a21fdc
said4ik <muxammadaminartikov@gmail.com>
2024-04-16 22:36:32 +0500

    Update Controller

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,9 +1,29 @@
 package controller;
 
+import model.User;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static Scanner scanStr = new Scanner(System.in);
+    public static Scanner scanInt = new Scanner(System.in);
+
+    public static User curretnUser = null;
+
+
+
     public static void main(String[] args) {
         
+    }
+    public static String inputStr(String hint) {
+        System.out.print(hint);
+        return scanStr.nextLine();
+    }
+
+    public static int input(String hint) {
+        System.out.print(hint);
+        return scanInt.nextInt();
     }
 }
